e866c85da5760601ad722bf4555fa1baf2765798
asitball <asit3310@gmail.com>
2020-11-03 21:18:20 +0600

    second day work added

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product.price);
+    console.log(props.product);
+    const {img, name,seller,price,stock} = props.product;
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+    
+        <div className='product'>
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+                
+                <h4 className='product-name'>{name}</h4>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                
+                 <p>${price}</p>
+                 
+                  <p><small>Only {stock} left in stock - Order soon</small></p>
+                  <button className='main-button'><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+            </div>
+            
         </div>
     );
 };
